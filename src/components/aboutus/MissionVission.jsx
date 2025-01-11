32ffb4f9bf28eaff2d1f368a0e67ba5486e42647
tanimtorikul <torikulswe@gmail.com>
2025-01-11 14:22:49 +0600

    added mission vision section in about us page

---
+++ src/components/aboutus/MissionVission.jsx
@@ -0,0 +1,30 @@
+const MissionVission = () => {
+  return (
+    <div className="flex gap-[130px]">
+      {/* mission */}
+      <div>
+        <h3 className="text-[36px] text-textGray leading-[44px] mb-[15px]">
+          Our Mission
+        </h3>
+        <p className="text-lg leading-[30px] text-textGrayV3">
+          Our mission is to empower travelers with peace of mind through
+          innovative, user-friendly, and comprehensive insurance solutions that
+          prioritize safety and convenience.
+        </p>
+      </div>
+      {/* vission */}
+      <div>
+        <h3 className="text-[36px] text-textGray leading-[44px] mb-[15px]">
+          Our Vision
+        </h3>
+        <p className="text-lg leading-[30px] text-textGrayV3">
+          To be the leading global travel insurance platform, empowering every
+          traveler with seamless access to reliable, affordable, and innovative
+          protection, ensuring peace of mind for journeys across the world.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MissionVission;
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,7 @@
 import CommonWrapper from "@/components/common/CommonWrapper";
 import OurStory from "@/components/aboutus/OurStory";
 import Topsection from "@/components/aboutus/topsection";
+import MissionVission from "@/components/aboutus/MissionVission";
 
 const AboutUs = () => {
   return (
@@ -10,6 +11,9 @@ const AboutUs = () => {
       {/* our story section */}
     <div className="mt-[87px]">
     <OurStory/>
+    </div>
+    <div className="py-[100px]">
+        <MissionVission/>
     </div>
     </CommonWrapper>
   );
