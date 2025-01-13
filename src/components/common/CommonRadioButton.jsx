9fb17edbd21401d2bb457db3eca2ef4719984b4e
tanimtorikul <torikulswe@gmail.com>
2025-01-13 10:18:47 +0600

    fixed the radio button click issues

--- src/components/common/CommonRadioButton.jsx
+++ src/components/common/CommonRadioButton.jsx
@@ -1,4 +1,3 @@
-import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const CommonRadioButton = ({ name, options, defaultValue }) => {
@@ -6,8 +5,10 @@ const CommonRadioButton = ({ name, options, defaultValue }) => {
     <RadioGroup defaultValue={defaultValue} name={name}>
       {options.map((option) => (
         <div key={option.value} className="flex items-center space-x-2">
-          <RadioGroupItem value={option.value} id={option.id} />
-          <Label htmlFor={option.id}>{option.label}</Label>
+          <label className="flex items-center space-x-2">
+            <RadioGroupItem value={option.value} id={option.id} />
+            <span>{option.label}</span>
+          </label>
         </div>
       ))}
     </RadioGroup>
