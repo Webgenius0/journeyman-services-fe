6ea11cc923874d200bf126a8bb38d05fb972f988
tanimtorikul <torikulswe@gmail.com>
2025-01-13 18:03:02 +0600

    fixed the pdf faqs

--- src/components/common/FaqPdf.jsx
+++ src/components/common/FaqPdf.jsx
@@ -8,7 +8,7 @@ const FaqPdf = () => {
         <h2 className="xl:text-xl xl:leading-[34px] text-center mb-[14px]">
           Click here for Covid-19 Coronavirus travel insurance FAQs
         </h2>
-        <div className="h-auto xl:h-[190px] border border-[#8CA2B4] py-[22px] px-16 flex flex-col justify-evenly items-center rounded-[5px]">
+        <div className="h-auto xl:h-[190px] border border-[#8CA2B4] py-[22px] px-16 flex flex-col xl:flex-row justify-evenly items-center rounded-[5px]">
           <div className="flex flex-col items-center xl:w-[200px]">
             <PdfIcon />
             <h3 className="text-sm xl:leading-7 text-textBlackV2 text-center">
