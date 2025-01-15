06156a0f13e0f6e489f42e9ce7700c4fd6f1b186
tanimtorikul <torikulswe@gmail.com>
2025-01-15 16:16:07 +0600

    implemented api in hero section

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -3,22 +3,24 @@ import CommonWrapper from "../common/CommonWrapper";
 import usersimg from "../../assets/users.png";
 import HeroIcon from "../../assets/Icons/HeroIcon";
 import PlaneIcon from "@/assets/Icons/PlaneIcon";
-import heroimg1 from "../../assets/hero-img-1.png";
-import heroimg2 from "../../assets/hero-img-2.png";
-import heroimg3 from "../../assets/hero-img-3.png";
 import EarthIcon from "@/assets/Icons/EarthIcon";
 import useFetchData from "@/hooks/api/useFetchData";
 import LoadingComponent from "../loaders/LoadingComponent";
 
 const HeroSection = () => {
   const { data, isLoading } = useFetchData("/home");
-  const info = data?.data?.cms.home_banner;
+  const info = data?.data?.cms?.home_banner;
+  const bannerImages = data?.data?.cms?.home_banners || [];
+
   console.log(info);
+  console.log("bannerimages", bannerImages);
+
   if (isLoading) return <LoadingComponent />;
+
   return (
     <CommonWrapper>
       <div className="flex flex-col xlg:flex-row xlg:items-center 2xl:items-start gap-[60px] 2xl:gap-[202px]">
-        {/* left side area with content */}
+        {/* Left side area with content */}
         <div className="w-full xlg:max-w-[546px]">
           <div className="flex justify-center xl:justify-start gap-4 items-center">
             <h3 className="font-medium text-textBlack">Fast</h3>
@@ -28,7 +30,7 @@ const HeroSection = () => {
             <h3 className="font-medium text-textBlack">Affordable</h3>
           </div>
           <h3 className="mt-4 xl:mt-0 text-2xl xl:text-6xl text-center xl:text-left text-textBlack font-bold xl:leading-[85px] mb-4">
-            Travel Insurance Instantly
+            {info?.title}
           </h3>
           <p className="text-textBlackV2 text-center xl:text-left xl:max-w-[330px]">
             {info?.description}
@@ -39,10 +41,10 @@ const HeroSection = () => {
               className="py-3 px-7"
               icon={<PlaneIcon />}
             >
-              Get a quote now
+              {info?.btn_text || "Learn More"}
             </CommonButton>
           </div>
-          {/* user img */}
+          {/* User img */}
           <div className="mt-6 xl:mt-[147px] flex flex-col jus xl:flex-row items-center gap-6 xl:gap-[60px]">
             <div className="xl:min-w-[312px] flex flex-col items-center xlg:block">
               <div>
@@ -53,36 +55,41 @@ const HeroSection = () => {
                 this Week
               </p>
             </div>
-            {/* icon */}
+            {/* Icon */}
             <div>
               <HeroIcon />
             </div>
           </div>
         </div>
 
-        {/* right side img */}
+        {/* Banner Images */}
         <div className="flex xlg:items-center gap-6">
-          {/* left imgs */}
-          <div>
-            {/* first and second */}
-            <div>
-              <img src={heroimg1} alt="" className="rounded-2xl" />
+          {bannerImages.map((image, index) => (
+            <div key={index}>
+              <div>
+                {/* First and second */}
+                <div>
+                  <img src={image} alt="" className="rounded-2xl" />
+                </div>
+                <div className="mt-6">
+                  <img src={image} alt="" className="rounded-2xl" />
+                </div>
+              </div>
+              {/* Right img */}
+              <div>
+                <div className="bg-[#E6EBEF] flex justify-center gap-2 items-center px-7 py-3 rounded-3xl mb-6">
+                  <EarthIcon />
+                  <p className="text-[12px] text-[#4B586B]">Alabama - Miami</p>
+                </div>
+                <div>
+                  <img src={image} alt="" className="rounded-2xl" />
+                </div>
+              </div>
             </div>
-            <div className="mt-6">
-              <img src={heroimg2} alt="" className="rounded-2xl" />
-            </div>
-          </div>
-          {/* right img */}
-          <div>
-            <div className="bg-[#E6EBEF] flex justify-center gap-2 items-center px-7 py-3 rounded-3xl mb-6">
-              <EarthIcon />
-              <p className="text-[12px] text-[#4B586B]">Alabama - Miami</p>
-            </div>
-            <div>
-              <img src={heroimg3} alt="" className="rounded-2xl" />
-            </div>
-          </div>
+          ))}
         </div>
+
+        {/* Right side images */}
       </div>
     </CommonWrapper>
   );
