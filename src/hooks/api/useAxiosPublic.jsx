c1eee313ed8e32e04128beab1ddfc7d02778b204
tanimtorikul <torikulswe@gmail.com>
2025-01-16 11:40:13 +0600

    added https instead of http

--- src/hooks/api/useAxiosPublic.jsx
+++ src/hooks/api/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://172.16.200.81:8010/api",
+  baseURL: "https://172.16.200.81:8010/api",
 });
 
 const useAxiosPublic = () => {
