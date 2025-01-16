d7b679c559d24a93da29a66e641f04824c7599f4
tanimtorikul <torikulswe@gmail.com>
2025-01-16 17:14:31 +0600

    implemented contact information api

--- src/components/contactus/ContactUs.jsx
+++ src/components/contactus/ContactUs.jsx
@@ -1,15 +1,22 @@
-
 import EmailIcon from "@/assets/Icons/EmailIcon";
 import LocationIcon from "@/assets/Icons/LocationIcon";
 import PhoneIcon from "@/assets/Icons/PhoneIcon";
 import CommonWrapper from "@/components/common/CommonWrapper";
+import useFetchData from "@/hooks/api/useFetchData";
 
 const ContactUs = () => {
+  const { data } = useFetchData("/settings");
+  console.log(data);
+  const contactInfomation = data?.data?.settings;
+  console.log(contactInfomation);
+
   return (
     <div className="xl:max-w-[294px] xlg:flex-1">
       <CommonWrapper noContainer>
-        <h2 className="text-xl xlg:text-[32px] font-bold text-black text-center xlg:text-left">Contact Us</h2>
-        
+        <h2 className="text-xl xlg:text-[32px] font-bold text-black text-center xlg:text-left">
+          Contact Us
+        </h2>
+
         <div className="mt-8 sm:block xlg:space-y-8  xl:block justify-between items-center">
           {/* email */}
           <div className="flex items-center gap-8 mb-4 xlg:mb-0">
@@ -17,8 +24,10 @@ const ContactUs = () => {
               <EmailIcon />
             </div>
             <div>
-              <h2 className="mb-2 text-base lg:text-lg xlg:text-2xl font-semibold xlg:font-bold">Email:</h2>
-              <p className="text-sm xlg:text-base">sales@shashh.com</p>
+              <h2 className="mb-2 text-base lg:text-lg xlg:text-2xl font-semibold xlg:font-bold">
+                Email:
+              </h2>
+              <p className="text-sm xlg:text-base">{contactInfomation.email}</p>
             </div>
           </div>
           {/* phone */}
@@ -27,8 +36,10 @@ const ContactUs = () => {
               <PhoneIcon />
             </div>
             <div>
-              <h2 className="mb-2 text-base lg:text-lg xlg:text-2xl font-semibold xlg:font-bold">Phone:</h2>
-              <p className="text-sm xlg:text-base">+966 54 873 0002</p>
+              <h2 className="mb-2 text-base lg:text-lg xlg:text-2xl font-semibold xlg:font-bold">
+                Phone:
+              </h2>
+              <p className="text-sm xlg:text-base">{contactInfomation.phone}</p>
             </div>
           </div>
           {/* location */}
@@ -37,8 +48,12 @@ const ContactUs = () => {
               <LocationIcon />
             </div>
             <div>
-              <h2 className="mb-2 text-base lg:text-lg xlg:text-2xl font-semibold xlg:font-bold">Address:</h2>
-              <p className="text-sm xlg:text-base">Dammam - Eastern Province Saudi Arabia</p>
+              <h2 className="mb-2 text-base lg:text-lg xlg:text-2xl font-semibold xlg:font-bold">
+                Address:
+              </h2>
+              <p className="text-sm xlg:text-base">
+                {contactInfomation.address}
+              </p>
             </div>
           </div>
         </div>
