c8e88f6a68de4bd3516049900ea932d88a1e9a9c
tanimtorikul <torikulswe@gmail.com>
2025-01-16 17:20:15 +0600

    added loading component in contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,21 +2,24 @@ import CommonTitle from "@/components/common/CommonTitle";
 import CommonWrapper from "@/components/common/CommonWrapper";
 import ContactForm from "@/components/contactus/ContactForm";
 import ContactUs from "@/components/contactus/ContactUs";
-
+import LoadingComponent from "@/components/loaders/LoadingComponent";
+import useFetchData from "@/hooks/api/useFetchData";
 
 const ContactPage = () => {
-    return (
-        <CommonWrapper version="lg">
-        <CommonTitle>We&apos;re Here to Help</CommonTitle>
-        <p className="mt-4 text-sm lg:text-base xl:text-xl text-textGray text-center xlg:text-left">
-          Have questions or need assistance? <br /> Reach out to us anytime.
-        </p>
-        <div className="flex flex-col justify-between xl:flex-row">
-          <ContactUs />
-          <ContactForm />
-        </div>
-      </CommonWrapper>
-    );
+  const { isLoading } = useFetchData("/settings");
+  if (isLoading) return <LoadingComponent />;
+  return (
+    <CommonWrapper version="lg">
+      <CommonTitle>We&apos;re Here to Help</CommonTitle>
+      <p className="mt-4 text-sm lg:text-base xl:text-xl text-textGray text-center xlg:text-left">
+        Have questions or need assistance? <br /> Reach out to us anytime.
+      </p>
+      <div className="flex flex-col justify-between xl:flex-row">
+        <ContactUs />
+        <ContactForm />
+      </div>
+    </CommonWrapper>
+  );
 };
 
 export default ContactPage;
