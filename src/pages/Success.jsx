cfb23ea92fe77605d3b7e428456cefb4d6e9a79d
tanimtorikul <torikulswe@gmail.com>
2025-01-28 18:25:22 +0600

    completed success page

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,22 +1,20 @@
-import Done from '@/assets/Icons/Done';
-import React from 'react';
-
+import Done from "@/assets/Icons/Done";
 const Success = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen ">
-            <div className="bg-white rounded-lg  w-full text-center">
-               <div className='flex justify-center'>
-               <Done  />
-               </div>
-                <h3 className="text-2xl font-semibold text-gray-800 mb-4">
-                    Your payment was successfully completed!
-                </h3>
-                <p className="text-lg text-gray-600">
-                    Your transaction is being processed.
-                </p>
-            </div>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <div>
+        <div className="flex justify-center">
+          <Done />
         </div>
-    );
+        <h3 className="xl:text-4xl font-semibold text-gray-800 mb-4">
+          Your payment was successfully completed!
+        </h3>
+        <p className="text-lg text-center text-gray-600">
+          Your transaction is being processed.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
