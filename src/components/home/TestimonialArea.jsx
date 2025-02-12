a6dbf173ec7b501ecb6f4d1b641ed16232960c31
tanimtorikul <torikulswe@gmail.com>
2025-02-12 16:33:28 +0600

    fixed testimonial issue

--- src/components/home/TestimonialArea.jsx
+++ src/components/home/TestimonialArea.jsx
@@ -8,16 +8,47 @@ import useFetchData from "@/hooks/api/useFetchData";
 const ReviewComponent = ({ info }) => {
   const metadata = JSON.parse(info.metadata || "{}");
   const rating = metadata.rating ? parseInt(metadata.rating, 10) : 0;
+
+  return (
+    <div className="w-[424px] space-y-8 rounded-xl bg-white px-6 py-12 shadow-md">
+      <div className="flex justify-center items-center gap-4">
+        <div className="flex items-center gap-1">
+          {[...Array(5)].map((_, index) => (
+            <span key={index}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 17 17"
+                fill={index < rating ? "#FF6C0A" : "#E0E0E0"}
+              >
+                <path d="M6.60178 4.10562C7.44622 2.59079 7.86843 1.83337 8.49967 1.83337C9.13092 1.83337 9.55313 2.59079 10.3976 4.10562L10.616 4.49753C10.856 4.928 10.976 5.14323 11.163 5.28525C11.3501 5.42726 11.5831 5.47997 12.0491 5.58541L12.4733 5.68139C14.1131 6.05241 14.933 6.23792 15.1281 6.86519C15.3231 7.49247 14.7642 8.14609 13.6463 9.45332L13.3571 9.79152C13.0394 10.163 12.8806 10.3487 12.8091 10.5785C12.7376 10.8083 12.7617 11.0561 12.8097 11.5517L12.8534 12.003C13.0224 13.7471 13.1069 14.6192 12.5962 15.0069C12.0856 15.3945 11.3179 15.0411 9.78256 14.3342L9.38535 14.1513C8.94906 13.9504 8.73091 13.8499 8.49967 13.8499C8.26844 13.8499 8.05029 13.9504 7.614 14.1513L7.21679 14.3342C5.68146 15.0411 4.91379 15.3945 4.40311 15.0069C3.89242 14.6192 3.97693 13.7471 4.14594 12.003L4.18966 11.5517C4.23769 11.0561 4.26171 10.8083 4.19025 10.5785C4.11879 10.3487 3.95996 10.163 3.64229 9.79152L3.35307 9.45332C2.23517 8.14609 1.67622 7.49247 1.87129 6.86519C2.06635 6.23792 2.88625 6.05241 4.52603 5.68139L4.95027 5.58541C5.41624 5.47997 5.64923 5.42726 5.8363 5.28525C6.02338 5.14323 6.14336 4.928 6.38332 4.49753L6.60178 4.10562Z" />
+              </svg>
+            </span>
+          ))}
+        </div>
+      </div>
+
+      <p className="text-base text-center leading-6 text-textGray">
+        {info.description || "No review available."}
+      </p>
+    </div>
+  );
 };
 
 ReviewComponent.propTypes = {
-  info: PropTypes.object,
+  info: PropTypes.shape({
+    metadata: PropTypes.string,
+    name: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+  }),
 };
 
 const TestimonialArea = () => {
   const { data } = useFetchData("/page/home");
   const reviews = data?.data?.cms?.testimonial || [];
-  console.log(reviews)
+
   return (
     <section>
       <CommonWrapper noContainer>
@@ -36,7 +67,7 @@ const TestimonialArea = () => {
               <Marquee gradient gradientColor="#eeefff" gradientWidth={120}>
                 <div className="mx-6 flex items-center gap-6">
                   {reviews.map((item, index) => (
-                    <ReviewComponent key={index} info={item} />
+                    <ReviewComponent key={item.id || index} info={item} />
                   ))}
                 </div>
               </Marquee>
@@ -48,7 +79,7 @@ const TestimonialArea = () => {
               >
                 <div className="mx-6 flex items-center gap-6">
                   {reviews.map((item, index) => (
-                    <ReviewComponent key={index} info={item} />
+                    <ReviewComponent key={item.id || index} info={item} />
                   ))}
                 </div>
               </Marquee>
